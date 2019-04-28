b11ca3f6c65bd5c6c0305f6a778b34af7aece3f3
hashy0917 <oohasi3939@gmail.com>
2019-04-29 00:35:43 +0900

    create react app.

---
+++ hello/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+	render () {
+		return <div className='App'>
+			<h2>塔を立てるときは、まず座って費用を計算しよう</h2>
+		</div>
+	}
+}
+
+
+export default App;
