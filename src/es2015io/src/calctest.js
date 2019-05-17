563e8e98ab9dfd1e67d15dc236c90e8788568ea6
hashy0917 <oohasi3939@gmail.com>
2019-05-17 11:51:04 +0900

    fix

--- src/es2015io/src/calctest.js
+++
@@ -1,9 +0,0 @@
-// 外部に公開する関数を定義
-export function add (a, b){
-	return a + b
-}
-
-export function mul (a, b){
-	return a * b
-}
-
--- src/es2015io/src/main.js
+++
@@ -1,6 +0,0 @@
-// 自作のモジュールを読み込む
-import { add, mul } from './calctest.js'
-
-// 取り込んだ関数を実行する
-console.log(add(2,3))
-console.log(mul(6,8))
