4dc777c2430e2bd0393ea2f7b59f7b5beb2268db
hashy0917 <oohasi3939@gmail.com>
2021-12-09 12:18:08 +0000

    add cloud-init & terraform counter

---
+++ terraform/nutanix/case3/inventry.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+if [ -e terraform/terraform.tfstate ]; then
+    ip=`cat terraform/terraform.tfstate | jq '.modules[].outputs[].value' | cut -d '"' -f 2`
+    cat << EOS
+{
+    "cloud_servers"  : [ $ip ]
+}
