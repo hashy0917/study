ba2b271b86b5aa5a1829ad9550e627d29c376379
hashy0917 <oohasi3939@gmail.com>
2021-12-09 14:35:03 +0000

    fix

--- terraform/nutanix/case3/inventry.sh
+++ terraform/nutanix/case3/inventry.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ -e $PWD/terraform.tfstate ]; then
-    ip=`cat $PWD/terraform.tfstate | jq '.resources[].instances[]' | jq '.attributes.nic_list_status[].ip_endpoint_list[].ip' 2> /dev/null`
+    ip=`cat $PWD/terraform.tfstate | jq '.resources[].instances[]' | jq '.attributes.nic_list_status[].ip_endpoint_list[].ip' 2> /dev/null | sed -e 's/"/ /g' | sed -ze 's/\n/,/g'`
     cat <<EOS
 {
     "cloud_servers"  : [ $ip ]
