11bacd44c0657a4dcd4601e2269016646f08ce1f
hashy0917 <oohasi3939@gmail.com>
2021-12-12 14:58:09 +0000

    add terraform sample

--- ansible/terraform/terraform-sample/inventry.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ -e $PWD/terraform.tfstate ]; then
-    ip=`cat $PWD/terraform.tfstate | jq '.resources[].instances[]' | jq '.attributes.nic_list_status[].ip_endpoint_list[].ip' 2> /dev/null | sed -e 's/"//g' | sed -ze 's/\n/,/g'`
-    cat <<EOS
-{
-    "cloud_servers"  : [ $ip ]
-}
-EOS
-fi
