0a68f48fffcabd3665ece3026477c5f22aa793cc
hashy0917 <oohasi3939@gmail.com>
2021-12-16 09:29:18 +0000

    masterとworkerを分けた

--- terraform/nutanix/kubernetes/inventory.sh
+++ terraform/nutanix/kubernetes/inventory.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 if [ -e $PWD/terraform.tfstate ]; then
-    ip=`cat $PWD/terraform.tfstate | jq '.resources[].instances[]' | jq '.attributes.nic_list_status[].ip_endpoint_list[].ip' 2> /dev/null | sed -e 's/"//g' | sed -ze 's/\n/,/g'`
+    master_ip=`cat $PWD/terraform.tfstate | jq '.resources[].instances[]' | jq '.attributes.nic_list_status[].ip_endpoint_list[].ip' 2> /dev/null | sed -e 's/"//g'| sed -ze 's/null\n//g' | head -n 1`
+    node_ip=`cat $PWD/terraform.tfstate | jq '.resources[].instances[]' | jq '.attributes.nic_list_status[].ip_endpoint_list[].ip' 2> /dev/null | sed -e 's/"//g'| sed -ze 's/null\n//g' | tail -n 2 | sed -ze 's/\n/,/g'`
     cat <<EOS
 {
-    "cloud_servers"  : [ $ip ]
+    "master"  : [ $master_ip ],
+    "worker"  : [ $node_ip ]
 }
 EOS
 fi
