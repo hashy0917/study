96f94cabd2c1b59dc5890999ab22e98ac0829f82
hashy0917 <oohasi3939@gmail.com>
2022-01-15 18:56:48 +0900

    wip

---
+++ terraform/cloudflare/case1/variables.tf
@@ -0,0 +1,21 @@
+# vsphere
+variable "vsphere_user" {
+  type = string
+}
+variable "vsphere_password" {
+  type = string
+}
+variable "vsphere_server" {
+  type = string
+}
+
+# cloudflare
+variable "cloudflare_email" {
+  type = string
+}
+variable "cloudflare_token" {
+  type = string
+}
+variable "cloudflare_domain" {
+  type = string
+}
